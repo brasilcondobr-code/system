c6a5d177ff6d858bb63301deda5b1c0ab2401a14
ocpcps <ocp.cps@gmail.com>
2025-12-04 11:27:00 -0300

    Atualização do Projeto

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Brasil Condo')
             ->colors([
                 'primary' => Color::Amber,
             ])
